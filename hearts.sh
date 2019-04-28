5ca474993861436914a471dd56df68388c4d128e
Jesse Millar <hellojessemillar@gmail.com>
2019-04-28 12:56:30 -0500

    Making jq write to a file

--- hearts.sh
+++ hearts.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+jq '.[.| length] |= . + {"date": "2010-01-07T19:55:99.999Z", "xml": "xml_samplesheet_2017_01_07_run_09.xml", "status": "OKKK", "message": "metadata loaded into iRODS successfullyyyyy"}' cards.json > test.json
+rm cards.json
+mv test.json cards.json
