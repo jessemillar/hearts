37ba97b781b15fa35361905a9a05154c1a6df382
Jesse Millar <hellojessemillar@gmail.com>
2019-04-28 15:42:50 -0500

    Utilizing selection menus for deck distributor

--- hearts.sh
+++ hearts.sh
@@ -18,15 +18,18 @@ CONDITION=0
 LIMITED_NUMBER=0
 LIMITED_TOTAL=0
 
+### Deck name
 # Mandatory fields are wrapped in while loops
 while [ -z ${NAME} ]; do
+	# TODO Check the database for similar decks before adding to the array
 	read -p "Deck name: " -e NAME
 done
 
-# TODO Check the database for similar decks before adding to the array
-read -p "Deck description: " -e DESCRIPTION
+### Deck image
+while [ -z ${IMAGE} ]; do
+	read -p "Deck image: " -e IMAGE
+done
 
-read -p "Deck image: " -e IMAGE
 # Download images provided via a URL
 if [[ $IMAGE =~ ^https?:\/\/.+ ]]; then
 	mkdir tmp
@@ -43,15 +46,52 @@ if [[ $IMAGE =~ ^https?:\/\/.+ ]]; then
 fi
 IMAGE=images/$IMAGE
 
+read -p "Deck description: " -e DESCRIPTION
 read -p "Deck homepage: " -e HOMEPAGE
-read -p "Deck distributor: " -e DISTRIBUTOR
+
+### Deck distributor
+echo "Deck distributor:"
+DISTRIBUTOR_OPTIONS=("Art of Play" "theory11" "Unknown" "Custom")
+select OPTION in "${DISTRIBUTOR_OPTIONS[@]}"
+do
+	case $OPTION in
+			"Art of Play")
+					DISTRIBUTOR="Art of Play"
+					break
+					;;
+			"theory11")
+					DISTRIBUTOR="theory11"
+					break
+					;;
+			"Unknown")
+					DISTRIBUTOR="Unknown"
+					break
+					;;
+			"Custom")
+					while [ -z ${NAME} ]; do
+						read -p "Custom distributor: " -e DISTRIBUTOR
+					done
+					break
+					;;
+			*) echo "Invalid option $REPLY";;
+	esac
+done
+
 read -p "Deck artist: " -e ARTIST
 read -p "Deck manufacturer: " -e MANUFACTURER
 read -p "Deck UPC: " -e UPC
 read -p "Notes: " -e NOTES
 read -p "How many owned: [1] " -e OWNED
-read -p "Deck condition: [0] " -e CONDITION
 
+### Deck condition
+echo "Deck condition:"
+CONDITION_OPTIONS=("Sealed" "Opened" "Used" "Destroyed")
+select OPTION in "${CONDITION_OPTIONS[@]}"; do
+	CONDITION=$OPTION
+	break
+done
+
+### Limited release decks
 read -p "Is deck part of a limited release? [y/N] " -e LIMITED
 # TODO Make this work
 if [ "$LIMITED" = "y" ]; then
@@ -65,4 +105,4 @@ if [ "$LIMITED" = "y" ]; then
 fi
 
 # Write to file
-jq --arg NAME "$NAME" --arg DESCRIPTION "$DESCRIPTION" --arg IMAGE "$IMAGE" --arg HOMEPAGE "$HOMEPAGE" --arg DISTRIBUTOR "$DISTRIBUTOR" --arg ARTIST "$ARTIST" --arg MANUFACTURER "$MANUFACTURER" --arg UPC "$UPC" --arg NOTES "$NOTES" --arg OWNED $OWNED --arg CONDITION $CONDITION --arg LIMITED_NUMBER $LIMITED_NUMBER --arg LIMITED_TOTAL $LIMITED_TOTAL '.[.| length] |= . + {"name": $NAME, "description": $DESCRIPTION, "image": $IMAGE, "homepage": $HOMEPAGE, "distributor": $DISTRIBUTOR, "artist": $ARTIST, "manufacturer": $MANUFACTURER, "upc": $UPC, "notes": $NOTES, "owned": $OWNED, "condition": $CONDITION, "limitedEdition": {"number": $LIMITED_NUMBER, "total": $LIMITED_TOTAL}}' cards.json > temp.json && mv -f temp.json cards.json
+jq --arg NAME "$NAME" --arg DESCRIPTION "$DESCRIPTION" --arg IMAGE "$IMAGE" --arg HOMEPAGE "$HOMEPAGE" --arg DISTRIBUTOR "$DISTRIBUTOR" --arg ARTIST "$ARTIST" --arg MANUFACTURER "$MANUFACTURER" --arg UPC "$UPC" --arg NOTES "$NOTES" --arg OWNED "$OWNED" --arg CONDITION "$CONDITION" --arg LIMITED_NUMBER "$LIMITED_NUMBER" --arg LIMITED_TOTAL "$LIMITED_TOTAL" '.[.| length] |= . + {"name": $NAME, "description": $DESCRIPTION, "image": $IMAGE, "homepage": $HOMEPAGE, "distributor": $DISTRIBUTOR, "artist": $ARTIST, "manufacturer": $MANUFACTURER, "upc": $UPC, "notes": $NOTES, "owned": $OWNED, "condition": $CONDITION, "limitedEdition": {"number": $LIMITED_NUMBER, "total": $LIMITED_TOTAL}}' cards.json > temp.json && mv -f temp.json cards.json
