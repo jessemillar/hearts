65932768d62850bc1e65d39e7fd6e1b1ddd7ff94
Jesse Millar <hellojessemillar@gmail.com>
2019-04-28 15:14:09 -0500

    Getting image downloading working

--- hearts.sh
+++ hearts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Set all variables to empties to allow for mandatory while loops below
+# Set non-defaulting variables to empties to allow for mandatory while loops below
 NAME=
 DESCRIPTION=
 IMAGE=
@@ -10,10 +10,10 @@ ARTIST=
 MANUFACTURER=
 UPC=
 NOTES=
-OWNED=
-CONDITION=
-LIMITED_NUMBER=
-LIMITED_TOTAL=
+OWNED=1
+CONDITION=0
+LIMITED_NUMBER=0
+LIMITED_TOTAL=0
 
 # Mandatory fields are wrapped in while loops
 while [ -z ${NAME} ]; do
@@ -22,19 +22,46 @@ done
 
 # TODO Check the database for similar decks before adding to the array
 read -p "Deck description: " -e DESCRIPTION
-# TODO Download images provided via a URL
+
 read -p "Deck image: " -e IMAGE
+# Download images provided via a URL
+if [[ $IMAGE =~ ^https?:\/\/.+ ]]; then
+	mkdir tmp
+	cd tmp
+	curl -JLO $IMAGE
+	TMP_FILENAME=$(ls | head -n 1)
+	FINAL_NAME=${NAME// /-}.png
+	FINAL_NAME_LOWERCASE=$(echo "$FINAL_NAME" | awk '{print tolower($0)}')
+	convert "$TMP_FILENAME" $FINAL_NAME
+	mv $FINAL_NAME ../images/$FINAL_NAME_LOWERCASE
+	IMAGE=$FINAL_NAME_LOWERCASE
+	cd ..
+	rm -rf tmp
+fi
+
+IMAGE=images/$IMAGE
+echo $IMAGE
+
 read -p "Deck homepage: " -e HOMEPAGE
 read -p "Deck distributor: " -e DISTRIBUTOR
 read -p "Deck artist: " -e ARTIST
 read -p "Deck manufacturer: " -e MANUFACTURER
 read -p "Deck UPC: " -e UPC
 read -p "Notes: " -e NOTES
-read -p "How many owned: " -i 1 -e OWNED
-read -p "Deck condition: " -i 0 -e CONDITION
-# TODO Ask if the deck is a limited edition
-read -p "Limited edition total count: " -i 0 -e LIMITED_TOTAL
-read -p "Limited edition number: " -i 0 -e LIMITED_NUMBER
+read -p "How many owned: [1] " -e OWNED
+read -p "Deck condition: [0] " -e CONDITION
+
+read -p "Is deck part of a limited release? [y/N] " -e LIMITED
+# TODO Make this work
+if [ "$LIMITED" = "y" ]; then
+	while [ -z ${LIMITED_TOTAL} ]; do
+		read -p "Limited edition total count: " -e LIMITED_TOTAL
+	done
+
+	while [ -z ${LIMITED_NUMBER} ]; do
+		read -p "Limited edition number: " -e LIMITED_NUMBER
+	done
+fi
 
 # Write to file
 jq --arg NAME "$NAME" --arg DESCRIPTION "$DESCRIPTION" --arg IMAGE "$IMAGE" --arg HOMEPAGE "$HOMEPAGE" --arg DISTRIBUTOR "$DISTRIBUTOR" --arg ARTIST "$ARTIST" --arg MANUFACTURER "$MANUFACTURER" --arg UPC "$UPC" --arg NOTES "$NOTES" --arg OWNED "$OWNED" --arg CONDITION $CONDITION --arg LIMITED_NUMBER $LIMITED_NUMBER --arg LIMITED_TOTAL $LIMITED_TOTAL '.[.| length] |= . + {"name": $NAME, "description": $DESCRIPTION, "image": $IMAGE, "homepage": $HOMEPAGE, "distributor": $DISTRIBUTOR, "artist": $ARTIST, "manufacturer": $MANUFACTURER, "upc": $UPC, "notes": $NOTES, "owned": $OWNED, "condition": $CONDITION, "limitedEdition": {"number": $LIMITED_NUMBER, "total": $LIMITED_TOTAL}}' cards.json > temp.json
